682988327f3e8206106ae20539ff4a0671b752b7
LaraMi92 <laraminski@hotmail.fr>
2022-02-03 17:56:39 +0100

    refactor scroll event

--- index.js
+++ index.js
@@ -1,58 +1,50 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
 
-    window.onscroll = function(){
+    window.onscroll = function () {
         let lastScrollTop = 0;
         let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    
+
         const aboutDiv = document.querySelector('#about');
-        const workOneText = document.querySelector('.section-content--work-1--content-text');
-        const workTwoText = document.querySelector('.section-content--work-2--content-text');
-        const workOneDiv = document.querySelector('.section-content--work-1--content');
-        const workTwoDiv = document.querySelector('.section-content--work-2--content');
-        const workThreeDiv = document.querySelector('.work-3');
-        const workFourDiv = document.querySelector('.work-4');
-        const workFiveDiv = document.querySelector('.work-5');
-        const workFiveText = document.querySelector('.work-5 .section-content--work-2--content-text');
-        const workThreeText = document.querySelector('.section-content--work-2--content-text:nth-child(2)');
-        const workFourText = document.querySelector('.section-content--work-2--content .work-4:first-child');
-    
+
+        const work1Text = document.querySelector('.section-content--work-1--content-text');
+        const work2Text = document.querySelector('.section-content--work-2--content-text');
+        const work1Div = document.querySelector('.section-content--work-1--content');
+        const work2Div = document.querySelector('.section-content--work-2--content');
+        const work3Div = document.querySelector('.work-3');
+        const work4Div = document.querySelector('.work-4');
+        const work5Div = document.querySelector('.work-5');
+        const work5Text = document.querySelector('.work-5 .section-content--work-2--content-text');
+        const work3Text = document.querySelector('.section-content--work-2--content-text:nth-child(2)');
+        const work4Text = document.querySelector('.section-content--work-2--content .work-4:first-child');
+
+        const divs = [work1Div, work2Div, work3Div, work4Div, work5Div];
+        const texts = [work1Text, work2Text, work3Text, work4Text, work5Text];
+
         //Scroll to bottom
-        if(scrollTop > 0 && lastScrollTop <= scrollTop) {
+        if (scrollTop > 0 && lastScrollTop <= scrollTop) {
             lastScrollTop = scrollTop;
-        if(aboutDiv.getBoundingClientRect().bottom <= 0){
-            workOneText.classList.remove('hidden');
-            workOneText.classList.add('tracking-in-contract');
+            if (aboutDiv.getBoundingClientRect().bottom <= 0) {
+                work1Text.classList.remove('hidden');
+                work1Text.classList.add('tracking-in-contract');
+            }
+            for (let i = 0; i < 4; i++) {
+                if (divs[i].getBoundingClientRect().bottom <= 0) {
+                    texts[i + 1].classList.remove('hidden');
+                    texts[i + 1].classList.add('tracking-in-contract');
+                }
+            }
+
+        } else {
+            lastScrollTop = scrollTop;
+            work1Text.classList.add('hidden');
+            work1Text.classList.remove('tracking-in-contract');
+            //Scroll to top
+            for (let i = 0; i < 4; i++) {
+
+                texts[i + 1].classList.add('hidden');
+                texts[i + 1].classList.remove('tracking-in-contract');
+
+            }
         }
-        if(workOneDiv.getBoundingClientRect().bottom <= 0){
-            workTwoText.classList.remove('hidden');
-            workTwoText.classList.add('tracking-in-contract');
-        }
-        if(workTwoDiv.getBoundingClientRect().bottom <= 0){
-            workThreeText.classList.remove('hidden');
-            workThreeText.classList.add('tracking-in-contract');
-        }
-        if(workThreeDiv.getBoundingClientRect().bottom <= 0){
-            workFourText.classList.remove('hidden');
-            workFourText.classList.add('tracking-in-contract');
-        }
-        if(workFourDiv.getBoundingClientRect().bottom <= 0){
-            workFiveText.classList.remove('hidden');
-            workFiveText.classList.add('tracking-in-contract');
-        }
-    } else {
-        lastScrollTop = scrollTop;
-        //Scroll to top
-        workOneText.classList.add('hidden');
-        workTwoText.classList.add('hidden');
-        workThreeText.classList.add('hidden');
-        workFourText.classList.add('hidden');
-        workFiveText.classList.add('hidden');
-        workOneText.classList.remove('tracking-in-contract');
-        workTwoText.classList.remove('tracking-in-contract');
-        workThreeText.classList.remove('tracking-in-contract');
-        workFourText.classList.remove('tracking-in-contract');
-        workFiveText.classList.remove('tracking-in-contract');
-    }
     }
 });
-
